18d6655df5ed6a111093884eb5d2017409300fac
Abhishek Dubey <abhi.dubey011999@gmail.com>
2020-08-19 18:12:26 +0530

    Updated model with hyperparameters
    Signed-off-by: Abhishek Dubey <abhi.dubey011999@gmail.com>

--- model.py
+++ model.py
@@ -1,6 +1,7 @@
 import torch.nn as nn
 import torch.nn.functional as F
 
+
 class _CNNLayerNorm(nn.Module):
     """Layer normalization built for cnns input"""
 
@@ -68,7 +69,14 @@ class _BidirectionalGRU(nn.Module):
 
 class SpeechRecognitionModel(nn.Module):
 
-    def __init__(self, n_cnn_layers, n_rnn_layers, rnn_dim, n_class, n_feats, stride=2, dropout=0.1):
+    def __init__(self,
+                 n_cnn_layers=3,
+                 n_rnn_layers=5,
+                 rnn_dim=512,
+                 n_class=29,
+                 n_feats=128,
+                 stride=2,
+                 dropout=0.1):
         super(SpeechRecognitionModel, self).__init__()
         n_feats = n_feats//2
         # cnn for extracting heirachal features
@@ -77,13 +85,13 @@ class SpeechRecognitionModel(nn.Module):
         # n residual cnn layers with filter size of 32
         self.rescnn_layers = nn.Sequential(*[
             _ResidualCNN(32, 32, kernel=3, stride=1,
-                        dropout=dropout, n_feats=n_feats)
+                         dropout=dropout, n_feats=n_feats)
             for _ in range(n_cnn_layers)
         ])
         self.fully_connected = nn.Linear(n_feats*32, rnn_dim)
         self.birnn_layers = nn.Sequential(*[
             _BidirectionalGRU(rnn_dim=rnn_dim if i == 0 else rnn_dim*2,
-                             hidden_size=rnn_dim, dropout=dropout, batch_first=i == 0)
+                              hidden_size=rnn_dim, dropout=dropout, batch_first=i == 0)
             for i in range(n_rnn_layers)
         ])
         self.classifier = nn.Sequential(
