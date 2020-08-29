c2dba1500a77893e7dc380e027797c024ffa0e09
Abhishek Dubey <abhi.dubey011999@gmail.com>
2020-08-29 17:04:09 +0530

    Major changes to command recognizer
    - Switched to dynamic threshold
    
    - Recognizer starts even if the logger is not available
    
    - Refactored for better functionality
    
    Signed-off-by: Abhishek Dubey <abhi.dubey011999@gmail.com>

--- recognize_command.py
+++ recognize_command.py
@@ -15,26 +15,34 @@ import torchaudio
 from model import SpeechRecognitionModel
 import data_processing as dp
 
-# Logger Import
 sys.path.append('..')
+
+# Logger Import
 from local_server.logger import Logger
+# Audio configs vars
+from audio_processing.audio_confs import *
+
+
+SHORT_NORMALIZE = (1.0/32768.0)
+TIMEOUT_LENGTH = 2
+MODEL_PATH = './command_model_trained.pth'
+CSV_PATH = './data/command_labels.csv'
+WAV_PATH = './temp.wav'
 
 # Logger Init
 logger = Logger()
-logger.connect()
+try:
+    logger.connect()
+    logger_connected = True
+except:
+    print('An exception occured while trying to connect to logger')
+    logger_connected = False
 
-# TODO: Dynamic Threshold
-THRESHOLD = 150
-FORMAT = pyaudio.paInt16
-SHORT_NORMALIZE = (1.0/32768.0)
-CHUNK = 1024
-CHANNELS = 1
-RATE = 16000
-S_WIDTH = 2
-TIMEOUT_LENGTH = 2
-MODEL_PATH = './command_model.pth'
-CSV_PATH = './data/command_labels.csv'
-WAV_PATH = './temp.wav'
+
+def _print_log(data):
+    print(data)
+    if logger_connected:
+        logger.log(data)
 
 
 class Listener:
@@ -57,13 +65,13 @@ class Listener:
         self.p = pyaudio.PyAudio()
         self.stream = self.p.open(format=FORMAT,
                                   channels=CHANNELS,
-                                  rate=RATE,
+                                  rate=S_RATE,
                                   input=True,
                                   output=True,
                                   frames_per_buffer=CHUNK)
 
     def record(self):
-        logger.log('\nAudio detected, recording now...\n')
+        _print_log('\nAudio detected, recording now...\n')
         rec = []
         current = time.time()
         end = time.time() + TIMEOUT_LENGTH
@@ -78,7 +86,7 @@ class Listener:
         return b''.join(rec)
 
     def listen(self):
-        logger.log('\nNow Listening...\n')
+        _print_log('\nNow Listening...\n')
         while True:
             input = self.stream.read(CHUNK)
             rms_val = self.rms(input)
@@ -86,65 +94,72 @@ class Listener:
                 return self.record()
 
     def write(self, filename, recording):
-        global wf
         wf = wave.open(filename, 'wb')
         wf.setnchannels(CHANNELS)
         wf.setsampwidth(self.p.get_sample_size(FORMAT))
-        wf.setframerate(RATE)
+        wf.setframerate(S_RATE)
         wf.writeframes(recording)
         wf.close()
 
 
-# Initialising a new model to recognize
-model = SpeechRecognitionModel()
+class CommandRecognizer():
+    def __init__(self):
+        # Initialising a new model to recognize
+        self.model = SpeechRecognitionModel()
 
-# Loading a pre-trained model
-model.load_state_dict(torch.load(MODEL_PATH))
-model.eval()
+        # Loading a pre-trained model
+        self.model.load_state_dict(torch.load(MODEL_PATH))
+        self.model.eval()
 
-# Loading command labels
-commands = list(pd.read_csv(CSV_PATH).iloc[:, 1])
+        # Loading command labels
+        self.commands = list(pd.read_csv(CSV_PATH).iloc[:, 1])
 
-while True:
+    def recognize(self):
+        # Listening and writing to a temp wav file
+        l = Listener()
+        wav_data = l.listen()
+        start_t = time.time()
+        l.write(WAV_PATH, wav_data)
 
-    # Listening and writing to a temp wav file
-    l = Listener()
-    wav_data = l.listen()
-    start_t = time.time()
-    l.write(WAV_PATH, wav_data)
+        # Reading wav file as a tensor
+        # TODO: Read directly from the recorded frames
+        waveform, _ = torchaudio.load_wav(WAV_PATH)
 
-    # Reading wav file as a tensor
-    waveform, _ = torchaudio.load_wav(WAV_PATH)
+        # Data-preprocecessing
+        specs = dp.preprocess_reco(waveform)
 
-    # Data-preprocecessing
-    specs = dp.preprocess_reco(waveform)
+        # Recognising
+        out = self.model(specs)
+        out = F.log_softmax(out, dim=2)
+        out = out.transpose(0, 1)
 
-    # Recognising
-    out = model(specs)
-    out = F.log_softmax(out, dim=2)
-    out = out.transpose(0, 1)
+        # Data-postprocessing
+        pred = dp.postprocess_reco(out.transpose(0, 1))
+        pred_str = ''.join(pred)
 
-    # Data-postprocessing
-    pred = dp.postprocess_reco(out.transpose(0, 1))
-    pred_str = ''.join(pred)
+        result_dict = {}
+        for command in self.commands:
+            result_dict[command] = dp.cer(command, pred_str)
 
-    result_dict = {}
-    for command in commands:
-        result_dict[command] = dp.cer(command, pred_str)
+        min_cer = min(result_dict.values())
 
-    min_val = min(result_dict.values())
+        # Results
+        accuracy = 1 - min_cer
+        result = ''.join([key for key in result_dict if result_dict[key] == min_cer])
 
-    # Results
-    if min_val < 0.5:
-        logger.log('Command Recognised -')
-        logger.log([key for key in result_dict if result_dict[key] == min_val])
-        logger.log('Accuracy Score - {:0.4f}\n'.format(1-min_val))
-    elif 0.5 < min_val < 0.7:
-        logger.log('Command Partially Recognised -')
-        logger.log([key for key in result_dict if result_dict[key] == min_val])
-        logger.log('Accuracy Score - {:0.4f}\n'.format(1-min_val))
-    else:
-        logger.log('Not able to reconize the command. Try again.')
+        if min_cer < 0.5:
+            _print_log('Command Recognised -')
+            _print_log(result)
+            _print_log('Accuracy Score - {:0.4f}\n'.format(accuracy))
 
-    logger.log("\n--- Total Recognition Time: %.4f seconds ---\n" %
-          (time.time() - start_t))
+        elif 0.5 < min_cer < 0.7:
+            _print_log('Command Partially Recognised -')
+            _print_log(result)
+            _print_log('Accuracy Score - {:0.4f}\n'.format(accuracy))
+
+        else:
+            _print_log('Not able to recognize the command. Try again.')
+            return None, accuracy
+
+        _print_log("\n--- Total Recognition Time: {:0.4f} seconds ---\n".format(time.time() - start_t))
+        return result, accuracy
